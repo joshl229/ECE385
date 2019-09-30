be3d27d6836d389b2c26384b46c0350aed0d4df2
joshl229 <joey32165@gmail.com>
2019-09-29 23:01:41 -0500

    Finished MUXes

--- lab6/mux.sv
+++ lab6/mux.sv
@@ -69,3 +69,72 @@ always_comb
 	
 endmodule
 
+module m_SR2MUX (
+						input logic [15:0] SR2,
+						input logic [15:0] SEXT,
+						input logic Select,
+						output logic [15:0] Output
+						);
+						
+always_comb
+	begin
+		case(Select)
+		
+		1'b0: 
+			Output = SR2;
+		1'b1:
+			Output = SEXT;
+		
+		endcase
+	end
+	
+endmodule 
+
+module m_ADDR1MUX (
+							input logic [15:0] PC,
+							input logic [15:0] SR1,
+							input logic Select,
+							output logic [15:0] Output
+						);
+						
+always_comb 
+	begin
+		case(Select)
+		
+		1'b0:
+			Output = PC;
+		1'b1:
+			Output = SR1;
+			
+		endcase
+	end
+	
+endmodule 
+
+module m_ADDR2MUX (
+							input logic [15:0] offset6,
+							input logic [15:0] PCoffset9,
+							input logic [15:0] PCoffset11,
+							input logic [1:0] Select,
+							output logic [15:0] Output
+						);
+						
+always_comb
+	begin
+		case(Select)
+		
+		2'b00:
+			Output = 16'b0000000000000000;
+		2'b01:
+			Output = offset6;
+		2'b10:
+			Output = PCoffset9;
+		2'b11:
+			Output = PCoffset11;
+			
+		endcase
+	end
+	
+endmodule 
+							
+							
